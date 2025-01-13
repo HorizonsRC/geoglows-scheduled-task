20b51bf81c97d5b56edfdb65535a44cbc96cd98b
nicmostert <nicolas.mostert@horizons.govt.nz>
2025-01-13 15:16:25 +1300

    fixed bat bug

--- geoglows_pull_reaches.bat
+++ geoglows_pull_reaches.bat
@@ -6,13 +6,13 @@ REM and run the geoglows_pull_reaches.py script
 REM ============================================================
 
 REM Path to the .env file
-set ENV_FILE=C:\Scripts\geoglows_pull_reaches\.env
+set ENV_FILE=C:\Scripts\geoglows-scheduled-task\.env
 
 REM Load environmen variables from .env file
 REM Each line in .env file should be in the format: KEY=VALUE
 REM See .env.example for an example .env file
 echo Loading environment variables from %ENV_FILE%...
-for /F "delims== tokens=1,* eol=#" %%i in (%filename%) do set %%i=%%~j
+FOR /F "delims== tokens=1,* eol=#" %%i in (%ENV_FILE%) do SET %%i=%%~j
 
 echo REPO_PATH=%REPO_PATH%
 echo REACH_FILE=%REACH_FILE%
